171d0daadcc1284690d040a7c582ded301cdc3f9
PhanKiet <tuankietvn@outlook.com>
2021-01-27 14:40:44 +0700

    push codes

---
+++ Cpp/app.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Test CPP Success" << endl;
+    return 0;
+}
---
+++ index.js
@@ -0,0 +1,54 @@
+const { exec } = require('child_process');
+
+async function sh(cmd) {
+  return new Promise(function (resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        resolve(stderr);
+      } else {
+        resolve({ stdout, stderr });
+      }
+    });
+  });
+}
+
+/**
+ *
+ * @param {String} language Having 3 languages: Cpp, JavaScript, Python (Case insensitive !)
+ */
+const main = async (language) => {
+  let path,
+    imgName,
+    flag = true;
+  switch (language.toLowerCase()) {
+    case 'cpp':
+      path = './Cpp';
+      imgName = 'cpp-runner';
+      break;
+    case 'javascript':
+      path = './JavaScript';
+      imgName = 'javascript-runner';
+      break;
+    case 'python':
+      path = './Python';
+      imgName = 'python-runner';
+      break;
+    default:
+      flag = false;
+  }
+  if (flag) {
+    await sh(`docker build -t ${imgName} ${path}`);
+    let { stdout } = await sh(`docker run ${imgName}`);
+    console.log(stdout);
+
+    console.log(
+      'CONTAINER PRUNE: ' + (await sh('docker container prune -f')).stdout,
+    );
+    console.log('IMAGE PRUNE: ' + (await sh('docker image prune -f')).stdout);
+    console.log('VOLUME PRUNE: ' + (await sh('docker volume prune -f')).stdout);
+  } else {
+    console.log('Unknown language found !');
+  }
+};
+
+main('cpp');
